bf41c0688ea738f6b591aa9e7fb6f7a20267b501
Max Bucknell <me@maxbucknell.com>
2019-06-10 20:12:25 +0100

    Initial Commit

---
+++ Colours/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Colours
+//
+//  Created by Max Bucknell on 10/06/2019.
+//  Copyright © 2019 Max Bucknell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
